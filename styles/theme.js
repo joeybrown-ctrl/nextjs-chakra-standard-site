72566937512398163320179ca0ac5ba890b95a46
Joey Brown <gjoey.brown@gmail.com>
2021-05-03 12:52:33 -0700

    Added breakpoints and expanded overrides within theme.js to include font weight and size.

--- styles/theme.js
+++ styles/theme.js
@@ -11,10 +11,38 @@ const fonts = {
     heading: 'Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji,"Segoe UI Emoji","Segoe UI Symbol'
 }
 
+
+// this sets our breakpoints, which are then set using the createBreakpoints hook imported from Chakra-UI. Breakpoints allow us to add mobile responsive styling. We use breakpoints instead of writing and then adding in media queries.
+const breakpoints = createBreakpoints({
+    sm: "40em",
+    md: "52em",
+    lg: "64em",
+    // we don't want extra-large, but we do have to set four breakpoints, so we're adding another large.
+    lg: "62em",
+})
+
 // this sets up our overrides
 const overrides = {
     ...chakraTheme, 
     fonts,
+    breakpoints,
+    fontWeights: {
+        normal: 300,
+        medium: 600,
+        bold: 700.
+    },
+    fontSizes: {
+        xs: "12px",
+        sm: "14px",
+        md: "16px",
+        lg: "18px",
+        xl: "20px",
+        "2xl": "24px",
+        "3xl": "28px",
+        "4xl": "36px",
+        "5xl": "48px",
+        "6xl": "64px",
+    }
 }
 
 // we store extendTheme that takes overrides as an argument, in customTheme
