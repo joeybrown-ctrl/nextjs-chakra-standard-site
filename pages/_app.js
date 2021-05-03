9d455f4042a6f3182b3267f83eb5f8827006639b
Joey Brown <gjoey.brown@gmail.com>
2021-05-03 13:04:16 -0700

    Set up DarkModeSwitch.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeProvider, useColorMode } from "@chakra-ui/react";
+import customTheme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider resetCSS theme={customTheme}>
+      <ColorModeProvider
+        options={{
+          // right now this is set to dark mode only at night
+          // changing useSystemColorMode to false, and initialColorMode to "dark", will set it to dark mode 24/7
+          initalColorMode: "light",
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
     </ChakraProvider>
   )
 }
